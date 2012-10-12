7ade9fccb7c6ec849c4103eb0c471e4d9c252375
David Rekowski <mail@rekowski.info>
2012-10-12 13:50:46 +0200

    use extracted autoloader

--- test/Athrois/AthroisTestCase.php
+++ test/Athrois/AthroisTestCase.php
@@ -1,21 +1,6 @@
 <?php
 
-// curtsy http://zaemis.blogspot.de/2012/05/writing-minimal-psr-0-autoloader.html
-
-spl_autoload_register(
-    function ($className)
-    {
-        $className = ltrim($className, "\\");
-        preg_match('/^(.+)?([^\\\\]+)$/U', $className, $match);
-        $className = str_replace("\\", "/", $match[1])
-            . str_replace(["\\", "_"], "/", $match[2])
-            . ".php";
-        $file = __DIR__ . '/../../src/' . $className;
-        if (file_exists($file) && is_readable($file)) {
-            include_once $file;
-        }
-    }
-);
+include_once __DIR__ . '/../../src/autoloader.php';
 
 class AthroisTestCase extends PHPUnit_Framework_TestCase {
 
